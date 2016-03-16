87467a063cb994986d6598fb3e08369596158fcc
lzj <564007786@qq.com>
2016-03-16 10:16:39 +0800

    搭建框架

---
+++ Project_github/Project_github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project_github
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 Lzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Project_github/Project_github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Project_github
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 Lzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
